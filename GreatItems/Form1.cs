2ec8cf1ba910362fb36754ce084041ad6bd94940
334052 <334052@CHS-232-13>
2021-10-21 09:39:18 -0500

    Add project files.

---
+++ GreatItems/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreatItems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int last = 10;
+            int starter = 1;
+            double sum = 0;
+            double alls = 0;
+
+            for (int i = starter; i <= last; i++)
+            {
+                sum += i;
+                alls += Math.Pow(i, 2);
+
+
+            }
+            if(Convert.ToInt64(Math.Pow))
+            { }
+
+            
+                
+
+            
+               
+
+            
+
+
+        }
+        
+    }
+}
